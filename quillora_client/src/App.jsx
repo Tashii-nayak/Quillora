db38fa260a4e22ff0ad2698f2f3333c117d9d607
Tashii-nayak <tashinayak16@gmail.com>
2025-07-08 06:40:35 +0530

    Update App.jsx

--- quillora_client/src/App.jsx
+++ quillora_client/src/App.jsx
@@ -1,12 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Signup from './pages/Signup';
+import Login from './pages/Login';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Link } from 'react-router-dom';
 
+//import '.style.css'; // Adjust the path as needed
+ 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
+      <Link to="/signup">JOIN/SIGN IN</Link>
+      <Link to="/login">LOGIN</Link>
     </BrowserRouter>
   );
 }
